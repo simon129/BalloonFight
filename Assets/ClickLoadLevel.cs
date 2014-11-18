6d5b0feefea08468e4ee7d67d067d3921e0428ac
Simon <simonc@gamesofa.com>
2014-11-18 18:46:47 +0800

    initial

---
+++ Assets/ClickLoadLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickLoadLevel : MonoBehaviour
+{
+    public string LevelName = "Game";
+
+    public void Click()
+    {
+        if (!string.IsNullOrEmpty(LevelName))
+            Application.LoadLevel(LevelName);
+    }
+}
